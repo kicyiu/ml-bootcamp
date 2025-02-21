436ae557b42790d8e037a1410365d7c67d5b4e01
Alberto Tsang <albertokicyiu@gmail.com>
2025-02-21 08:19:34 -0300

    my first commit

---
+++ life.py
@@ -0,0 +1,10 @@
+import math
+
+theAnswer = 42
+
+def quote_marvin():
+    print('I have calculated your chance of survival, but I don\'t think you\'ll like it.')
+
+def square_root(x):
+    return math.sqrt(x)
+    
